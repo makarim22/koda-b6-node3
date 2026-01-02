1d98eb367d25a2f2805d791a689284c20f7e019f
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2026-01-02 16:22:20 +0700

    Update convert2.js

--- convert2.js
+++ convert2.js
@@ -1,15 +1,19 @@
 
 function convertNew (date){
     if (typeof  date !== 'string'){
+        console.log('input berupa' + typeof date);
         return "Input Tidak Valid: Harus berupa string"
     }
-    const dateNew = date.split('-');
+    const dateNew = date.split('-').map(Number);
     const [day, month, year] = dateNew;
 
-    if (day < 1 || day > 31 || month < 1 || month > 12 || year < 1970 || year > 2026) {
+    if (day < 1 || day > 31 || month < 1 || month > 12) {
         return "Input Tanggal Tidak Valid: Format Tanggal Harus Tanggal-Bulan-Tahun dan di rentang yang valid";
     }
     return `${day}/${month}/${year}`
 }
 
+convertNew('2023-01-01')
+// convertNew(true)
+// convertNew(1)
 module.exports = convertNew
